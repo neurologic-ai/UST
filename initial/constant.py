bcf79db2dd17ffb978fdb50c9a73fa6bfb3e209f
Sujan Das <sujan.das@neurologicai.com>
2024-10-16 19:22:32 +0530

    validation column names added

--- initial/constant.py
+++ initial/constant.py
@@ -5,7 +5,7 @@ GREEN = '\033[92m'
 RED = '\033[91m'
 RESET = '\033[0m'
 
-EXPECTED_COLS = {
+EXPECTED_PROCESSED_COLS = {
     'Session_id': 'int64',
     'Date_time': 'object',
     'Product_name': 'object',
@@ -17,3 +17,11 @@ US_HOLIDAYS = holidays.US(years = range(2015, 2035))  # Adjust years based on yo
 
 SHOP_LOCATION = 'Jackson Hole Airport'
 DATE_COL = 'Date_time'
+
+EXPECTED_CATEGORY_COLS = {
+    'Product_name': 'object',
+    'Category': 'object',
+    'Subcategory': 'object',
+    'Subcategory2': 'object',
+    'Subcategory2' : 'object'
+}

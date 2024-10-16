e333592dac7a2f5199959253236d2a63a142f669
Sujan Das <sujan.das@neurologicai.com>
2024-10-16 19:22:55 +0530

    Added category data validation

--- initial/data_validation.py
+++ initial/data_validation.py
@@ -1,21 +1,24 @@
 import pandas as pd
-from initial.constant import PROCESSED_DATA_PATH, GREEN, RED, RESET, EXPECTED_COLS
+from initial.constant import  GREEN, RED, RESET, EXPECTED_PROCESSED_COLS, EXPECTED_CATEGORY_COLS
 
-def validate():
-
-    try:
-        df = pd.read_csv(PROCESSED_DATA_PATH)
-    except:
-        print("Failed to read the dataset")
-        return False
-
+def validate(df1, df2):
     problem = False
-    for column, expected_dtype in EXPECTED_COLS.items():
-        if column not in df.columns:
+    for column, expected_dtype in EXPECTED_PROCESSED_COLS.items():
+        if column not in df1.columns:
             print(f"{RED}Column '{column}' is missing.{RESET}")
             problem = True
-        elif df[column].dtype != expected_dtype:
-            print(f"{RED}Column '{column}' has incorrect type. Expected {expected_dtype} but got {df[column].dtype}.{RESET}")
+        elif df1[column].dtype != expected_dtype:
+            print(f"{RED}Column '{column}' has incorrect type. Expected {expected_dtype} but got {df1[column].dtype}.{RESET}")
+            problem = True
+        else:
+            print(f"{GREEN}Column '{column}' exists and has the correct type ({expected_dtype}).{RESET}")
+
+    for column, expected_dtype in EXPECTED_CATEGORY_COLS.items():
+        if column not in df2.columns:
+            print(f"{RED}Column '{column}' is missing.{RESET}")
+            problem = True
+        elif df2[column].dtype != expected_dtype:
+            print(f"{RED}Column '{column}' has incorrect type. Expected {expected_dtype} but got {df2[column].dtype}.{RESET}")
             problem = True
         else:
             print(f"{GREEN}Column '{column}' exists and has the correct type ({expected_dtype}).{RESET}")
@@ -25,6 +28,7 @@ def validate():
     else:
         print("Data Validation Successful")
         return True
+
 
 
 

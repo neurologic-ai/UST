2615a19e93b4fcfba4ad23aab066d454846b5e58
Sujan Das <sujan.das@neurologicai.com>
2024-10-16 19:23:06 +0530

    Added setup api

--- routes/recommendation_route.py
+++ routes/recommendation_route.py
@@ -12,6 +12,9 @@ from db.singleton import get_engine
 from models.db import Association_collection
 from routes.user_route import PermissionChecker
 from utils.helper import get_association_recommendations, get_calendar_recommendation, get_popular_recommendation, get_time_recommendation, get_weather_recommendation
+from configs.constant import PROCESSED_PATH, CATEGORY_PATH
+from initial.data_validation import validate
+from setup import run_models_and_store_outputs
 
 router = APIRouter()
 
@@ -74,3 +77,33 @@ async def recommendation(
     aggregator = Aggregation(all_recommendations, cart_items, categories_dct, current_hr)
     final_recommendations = aggregator.get_final_recommendations()
     return final_recommendations
+
+from fastapi import FastAPI, UploadFile, File
+import pandas as pd 
+# pip install python-multipart
+
+@router.post("/setup")
+async def upload_csvs(
+    processed: UploadFile = File(...), 
+    categories: UploadFile = File(...)
+):
+    # Read both files into pandas DataFrames
+    df1 = pd.read_csv(processed.file)
+    df2 = pd.read_csv(categories.file)
+    # Lets see is the input files are in correct format
+    validation = validate(df1, df2)
+    if not validation:
+        print(f"Error: Validation failed, please try again with correct data format.")
+        return {"Error": "Validation failed, please try again with correct data format."}
+
+    # Store the input files
+    df1.to_csv(PROCESSED_PATH, index = False)
+    df2.to_csv(CATEGORY_PATH, index = False)
+    print("Data is stored successfully")
+    try:
+        run_models_and_store_outputs()
+    except:
+        return {"Error": "Failed to run the recomendation model."}
+    
+    # Return the shape as a JSON response
+    return {"message": "Set up has been completed, now you can safely run the recommendation API."}

df965a29a1e63b3cd65fe9982fcdd90efed9aee4
sami281 <iamsd281@gmail.com>
2025-06-30 23:11:43 +0530

    bug

--- models/hepler.py
+++ models/hepler.py
@@ -30,20 +30,19 @@ class Product:
 
 class Aggregation:
     def __init__(self, reco_list, cart_items, categories, current_hour, weather,
-                 excluded_subcategories = EXCLUDE_SUBCATEGORIES,
-                 strict_category_rules = STRICT_CATEGORY_RULES,
-                 mono_subcategories = MONO_CATEGORIES,
-                 cross_subcategories = CROSS_CATEGORIES,
-                 time_slots = TIME_SLOTS,
-                 max_subcategory_limit = MAX_SUBCATEGORY_LIMIT):
+                 excluded_subcategories=EXCLUDE_SUBCATEGORIES,
+                 strict_category_rules=STRICT_CATEGORY_RULES,
+                 mono_subcategories=MONO_CATEGORIES,
+                 cross_subcategories=CROSS_CATEGORIES,
+                 time_slots=TIME_SLOTS,
+                 max_subcategory_limit=MAX_SUBCATEGORY_LIMIT):
         
         self.reco_list = reco_list
         self.cart_items = cart_items
-        
         self.categories = categories
         self.current_hour = current_hour
         self.weather = weather.lower()
-        
+
         self.excluded_subcategories = excluded_subcategories
         self.strict_category_rules = strict_category_rules
         self.mono_subcategories = mono_subcategories
@@ -52,120 +51,108 @@ class Aggregation:
         self.max_subcategory_limit = max_subcategory_limit
 
     def exclude_cart_items(self):
-        """Remove items already in the cart from the recommendation list."""
         self.reco_list = [p for p in self.reco_list if p not in self.cart_items]
-        # logger.debug(self.reco_list)
-        
-    def exclude_obvious_categories(self):
-        """Remove products from excluded subcategories."""
-        self.reco_list = [
-            p for p in self.reco_list 
-            if self.categories[p.strip()].subcategory not in self.excluded_subcategories
-        ]
-        # logger.debug(self.reco_list)
-    
 
-    def remove_non_timely_products(self):
-        """Exclude products that are not appropriate for the current time."""
+    def exclude_obvious_categories(self):
         self.reco_list = [
             p for p in self.reco_list
-            if self.categories[p.strip()].timing not in self.time_slots
-            or self.current_hour in range(*self.time_slots[self.categories[p.strip()].timing])
+            if self.categories.get(p.strip()) and self.categories[p.strip()].subcategory not in self.excluded_subcategories
         ]
-        # logger.debug(self.reco_list)
+
+    def remove_non_timely_products(self):
+        self.reco_list = [
+            p for p in self.reco_list
+            if self.categories.get(p.strip()) and (
+                self.categories[p.strip()].timing not in self.time_slots
+                or self.current_hour in range(*self.time_slots[self.categories[p.strip()].timing])
+            )
+        ]
+
     def prioritize_associations(self):
-        """Prioritize and filter products based on cart items."""
         if not self.cart_items:
             return
 
-        # cart_subcats = {self.categories[p.strip()].subcategory for p in self.cart_items if self.categories[p.strip()].subcategory in self.mono_subcategories}
         cart_subcats = {
             self.categories[p.strip()].subcategory
             for p in self.cart_items
+            if self.categories.get(p.strip())
         }
 
-        # Remove items in the same mono subcategory
         self.reco_list = [
-            p for p in self.reco_list 
-            if self.categories[p.strip()].subcategory not in cart_subcats
+            p for p in self.reco_list
+            if self.categories.get(p.strip()) and self.categories[p.strip()].subcategory not in cart_subcats
         ]
 
-        # Remove conflicting items based on strict category rules
-        cart_cats = {self.categories[p.strip()].category for p in self.cart_items}
+        cart_cats = {
+            self.categories[p.strip()].category
+            for p in self.cart_items
+            if self.categories.get(p.strip())
+        }
+
         conflicting_categories = set()
         for cart_cat in cart_cats:
             if cart_cat in self.strict_category_rules:
                 conflicting_categories.update(self.strict_category_rules[cart_cat])
-    
+
         self.reco_list = [
-            p for p in self.reco_list 
-            if self.categories[p.strip()].category not in conflicting_categories
+            p for p in self.reco_list
+            if self.categories.get(p.strip()) and self.categories[p.strip()].category not in conflicting_categories
         ]
 
-        # Prioritize cross-subcategory recommendations
-        last_cart_item = self.cart_items[-1]
-        cart_subcat1 = self.categories[last_cart_item.strip()].subcategory
-        if cart_subcat1 in self.cross_subcategories:
-            prioritized_items = [p for p in self.reco_list 
-                                 if self.categories[p.strip()].subcategory in self.cross_subcategories[cart_subcat1]]
-
-            
-            self.reco_list = prioritized_items + [item for item in self.reco_list if item not in prioritized_items]
-        # logger.debug(self.reco_list)
+        last_cart_item = self.cart_items[-1].strip()
+        if self.categories.get(last_cart_item):
+            cart_subcat1 = self.categories[last_cart_item].subcategory
+            if cart_subcat1 in self.cross_subcategories:
+                prioritized_items = [
+                    p for p in self.reco_list
+                    if self.categories.get(p.strip()) and self.categories[p.strip()].subcategory in self.cross_subcategories[cart_subcat1]
+                ]
+                self.reco_list = prioritized_items + [item for item in self.reco_list if item not in prioritized_items]
 
     def limit_same_category_occurrences(self):
-        """Limit the number of products from the same category."""
         category_count = defaultdict(int)
         refined_list = []
 
         for p in self.reco_list:
-            subcategory = self.categories[p.strip()].subcategory
-            if category_count[subcategory] < self.max_subcategory_limit:
-                category_count[subcategory] += 1
-                refined_list.append(p)
+            category = self.categories.get(p.strip())
+            if category:
+                subcategory = category.subcategory
+                if category_count[subcategory] < self.max_subcategory_limit:
+                    category_count[subcategory] += 1
+                    refined_list.append(p)
 
         self.reco_list = refined_list
-        # logger.debug(self.reco_list)
-    
+
     def exclude_shorter_product_names(self):
         self.reco_list = [item for item in self.reco_list if len(item) > 1]
-        # logger.debug(self.reco_list)
 
     def filter_by_weather(self):
-        """Filter recommendations based on weather conditions."""
         filters = WEATHER_FILTERS.get(self.weather, {})
         avoid_subcats = set(filters.get("avoid", []))
         prefer_subcats = set(filters.get("prefer", []))
 
-        # Avoid unwanted subcategories
         self.reco_list = [
-            p for p in self.reco_list 
-            if self.categories[p.strip()].subcategory not in avoid_subcats
+            p for p in self.reco_list
+            if self.categories.get(p.strip()) and self.categories[p.strip()].subcategory not in avoid_subcats
         ]
 
-        # Prioritize preferred ones
         preferred = [
-            p for p in self.reco_list 
-            if self.categories[p.strip()].subcategory in prefer_subcats
+            p for p in self.reco_list
+            if self.categories.get(p.strip()) and self.categories[p.strip()].subcategory in prefer_subcats
         ]
         others = [
-            p for p in self.reco_list 
-            if self.categories[p.strip()].subcategory not in prefer_subcats
+            p for p in self.reco_list
+            if self.categories.get(p.strip()) and self.categories[p.strip()].subcategory not in prefer_subcats
         ]
 
         self.reco_list = preferred + others
 
-
     def get_final_recommendations(self):
-        """Execute all filtering and return the final list of recommended products."""
         self.exclude_cart_items()
         self.exclude_obvious_categories()
         # self.remove_non_timely_products()
         self.prioritize_associations()
         self.limit_same_category_occurrences()
         self.exclude_shorter_product_names()
-        self.filter_by_weather() 
-        # logger.debug(self.reco_list)
+        self.filter_by_weather()
         return self.reco_list
-        
-    

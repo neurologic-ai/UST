ebfbf52717a6b6bd004aaef183c02db4bd83856f
sami281 <iamsd281@gmail.com>
2025-07-03 15:30:55 +0530

    logger added for redis

--- db/redis_client.py
+++ db/redis_client.py
@@ -1,9 +1,12 @@
 # redis_client.py
+from loguru import logger
 import redis
 from configs.manager import settings
 
 
 def get_redis_client() -> redis.Redis:
+    logger.debug(settings.REDIS_HOST)
+    logger.debug(settings.REDIS_PORT)
     return redis.Redis(
         host=settings.REDIS_HOST,
         port=settings.REDIS_PORT,

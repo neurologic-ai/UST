1caf6101e1f3300c77f92d8505d5e011a7d61fb9
sami281 <iamsd281@gmail.com>
2025-07-03 13:40:16 +0530

    bug

--- db/redis_client.py
+++ db/redis_client.py
@@ -4,4 +4,10 @@ from configs.manager import settings
 
 
 def get_redis_client() -> redis.Redis:
-    return redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
+    return redis.Redis(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        db=0,
+        socket_connect_timeout=0.3,  # 300ms to establish connection
+        socket_timeout=0.5           # 500ms max wait for response
+    )
